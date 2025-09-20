23d381c7bdcb303e20f11bcaf76853f44a70bfd4
Carepred <carepred.ballos88@gmail.com>
2025-09-20 15:15:13 +0800

    Create return function the accepts parameter from the agrument

--- banana.py
+++ banana.py
@@ -1,1 +1,14 @@
 print("I eat a 🍌 and another 🍌 ")
+
+
+
+def banana(fruit):
+
+    return f"I eat a {fruit}"
+
+def main():
+
+    print(banana("Big banaa"))
+
+if __name__ == "__main__":
+    main()
